ff8a4285295ec4e8bbc521d572b504bc3b4b527d
Eric Winiecke <ewiniecke@gmail.com>
2017-07-10 23:13:18 +0000

    First Commit

---
+++ apps/exchange_platform/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExchangePlatformConfig(AppConfig):
+    name = 'exchange_platform'
---
+++ apps/exchange_platform/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, reverse, HttpResponse
+from django.contrib import messages
+from . models import *
+
+def index(request):
+    print("*"*50)
+    return render(request, 'exchange_platform/index.html')
