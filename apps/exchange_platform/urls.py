63a8fe92f81eb590b80f6f35c1265fcbf6769994
Eric Winiecke <ewiniecke@gmail.com>
2017-07-11 18:11:17 +0000

    CSS and Login Reg

--- apps/exchange_platform/urls.py
+++ apps/exchange_platform/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^login_page$', views.login_page, name="loginpage"),
+    url(r'^register$',views.register, name='registerpage'),
+    url(r'^logout$', views.logout, name="logout"),
 
 ]
